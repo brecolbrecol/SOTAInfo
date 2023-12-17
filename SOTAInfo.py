ac1779840402a2ea7a5a58c1393d382402ae0b17
j 🥦 <me@jorge.red>
2023-12-17 22:39:07 +0100

    style: rename, callsigns → activators

--- SOTAInfo.py
+++ SOTAInfo.py
@@ -11,32 +11,32 @@ class SOTAInfo(object):
         pathlib.Path(cache_dir).mkdir(parents=True, exist_ok=True)  # ensures tempdir exists
         self.cache_dir = cache_dir
         self.associationID = associationID
-        self._callsigns = self.init_callsigns()
+        self._activators = self.init_activators()
 
     @property
-    def Callsigns(self):
+    def Activators(self):
         """
         {'Position', 'UserID', 'Callsign', 'Username', 'Points', 'BonusPoints', 'Summits', 'totalPoints', 'Average'}
         :return: json 
         """
-        return self._callsigns
+        return self._activators
 
-    def init_callsigns(self):
-        """Itializes callsign attribute"""
-        cache_file_path = pathlib.Path(self.cache_dir + "callsigns" + str(self.associationID) + ".json")
+    def init_activators(self):
+        """Itializes activators attribute"""
+        cache_file_path = pathlib.Path(self.cache_dir + "activators" + str(self.associationID) + ".json")
         if (cache_file_path.exists()):
-            with cache_file_path.open(mode="r", encoding="utf-8") as f_callsigns:
+            with cache_file_path.open(mode="r", encoding="utf-8") as f_activators:
                 try:
-                    return json.load(f_callsigns)
+                    return json.load(f_activators)
                 except Exception:
                     traceback.print_exc()
 
-        callsigns = self.fetch_callsigns()
-        with cache_file_path.open(mode="w", encoding="utf-8") as f_callsigns:
-            json.dump(callsigns, f_callsigns, indent=2)
-        return callsigns
+        activators = self.fetch_activators()
+        with cache_file_path.open(mode="w", encoding="utf-8") as f_activators:
+            json.dump(activators, f_activators, indent=2)
+        return activators
 
-    def fetch_callsigns(self):
+    def fetch_activators(self):
         print("Fetching association " + str(self.associationID) + " call signs...", end='')
         api_data = urllib.request.urlopen("https://api-db.sota.org.uk/admin/activator_roll?associationID=" +
                                           str(self.associationID)).read()
@@ -46,4 +46,4 @@ class SOTAInfo(object):
 
 if __name__ == "__main__":
     cumbres_activadas = SOTAInfo()
-    datos = cumbres_activadas.Callsigns
+    datos = cumbres_activadas.Activators
