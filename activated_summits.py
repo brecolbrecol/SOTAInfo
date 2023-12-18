c11e74858caf44a259096218a7bc5067f6429e85
j 🥦 <me@jorge.red>
2023-12-18 22:07:42 +0100

    feat: results table showing how many times reference was activated

--- activated_summits.py
+++ activated_summits.py
@@ -1,11 +1,37 @@
 #!/usr/bin/env python3
 from lib.SOTAInfo import SOTAInfo
 
+"""
+Given a list of activators and a list of references, count how many times each reference was activated by each activator
+"""
+
+def activators_christmas():
+    return ["EA4GDK","EA4GZU","EA4HCF","EA4HGT","EA4DE","EA4HIH","EA4HTO","EA4FUA","EB4FJV","EA4HNQ","EA4HSS","EA4HFO"]
+
+def proposed_references():
+    return ["EA1/SG-017", "EA1/SG-005", "EA4/MD-017", "EA4/MD-045"]
 
 if __name__ == "__main__":
-    cumbres_activadas = SOTAInfo()
-    data = cumbres_activadas.Activators
-    ea4hfo = data['EA4HFO']
-    print("Points: " + str(ea4hfo.Points))
-    blah = ea4hfo.activated_references(year=2023)
-    print("Done.")
+    summits_count = {}
+    sota_info = SOTAInfo()
+    activators_all = sota_info.Activators
+
+    for activator in activators_christmas():
+        try:
+            activated_references = activators_all[activator].activated_references(year=2023)
+            for activated_reference in activated_references:
+                reference_name = activated_reference['Summit']
+                if not reference_name in summits_count:
+                    summits_count[reference_name] = {'count': 1, 'activators': activator}
+                elif activator not in summits_count[reference_name]['activators']:
+                    summits_count[reference_name]['count'] += 1
+                    summits_count[reference_name]['activators'] += ', ' + activator
+                else:
+                    print(activator + " repeated " + reference_name)
+        except:
+            print(activator + " not found in SOTA database")
+
+    print("\n== Results ==")
+    for reference, data in sorted(summits_count.items(), key=lambda kv: kv[1]['count']):
+        if reference.split()[0] in proposed_references():
+            print(str(data['count']) + " - " + reference + " -> " + data['activators'])
