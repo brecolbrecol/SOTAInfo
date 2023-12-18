30358abe6f5b84f865f8f03a4460844fc1cb874b
j 🥦 <me@jorge.red>
2023-12-18 18:14:22 +0100

    enh: method for fetching summits activated by call sign

--- lib/Activator.py
+++ lib/Activator.py
@@ -1,9 +1,33 @@
+import urllib
+import json
+
 class Activator:
     def __init__(self, attributes: dict):
+        self._summits = None
         for key, value in attributes.items():
             #ToDo: search for specific attributes
+            # {'Position', 'UserID', 'Callsign', 'Username', 'Points', 'BonusPoints', 'Summits', 'totalPoints', 'Average'}
             setattr(self, key, value)
-            
+
+    @property
+    def summits(self, year: int = None):
+        if self._summits is None:
+            self._summits = self.fetch_summits(year)
+        return self._summits
+
+    def fetch_summits(self, year: int = None):
+        msg = "Fetching summits activated by " + self.Callsign
+        url = "https://api-db.sota.org.uk/admin/activator_log_by_id?id=" + str(self.UserID)
+        if year is not None:
+            url += "&year=" + str(year)
+            msg += " in " + str(year)
+
+        print( msg + "...", end='')
+        api_data = urllib.request.urlopen(url).read()
+        print(" done!")
+
+        return json.loads(api_data.decode('utf-8'))
+
     @classmethod
     def dict_from_json(cls, activators_json: list):
         """Converts a list of activators into a dict of activators, using call sing as key"""
